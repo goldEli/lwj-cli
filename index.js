e30ae3217f78afd24758dadae3357193292d0fd1
miaoyu <miaoyu2009@qq.com>
2021-05-19 16:04:47 +0800

    feat: add arguments

--- index.js
+++ index.js
@@ -1,5 +1,45 @@
-import {createProject} from "./src/createProject"
+import { createProject } from "./src/createProject";
+import arg from "arg";
+
+function parseArgumentsIntoOptions(rawArgs) {
+  const args = arg(
+    {
+      "--npm": Boolean,
+      "--help": Boolean,
+      "-n": "--npm",
+      "-h": "--help",
+    },
+    {
+      argv: rawArgs.slice(2),
+    }
+  );
+  return {
+    useNpm: args["--npm"],
+    help: args["--help"],
+    command: args._[0]
+  };
+}
 
 export async function run(args) {
-  await createProject();
+  let options = parseArgumentsIntoOptions(args);
+  if (options.help) {
+    console.log("Usage: lwj <command> [options]");
+    console.log();
+    console.log("Options:");
+    console.log("  --help          output usage information");
+    console.log("  --npm          use npm (use yarn by default)");
+    console.log();
+    console.log("Commands:");
+    console.log("  create         创建渐进式引入taro项目");
+    return;
+  }
+  switch (options.command) {
+    case "create":
+      await createProject(options);
+
+      break;
+
+    default:
+      break;
+  }
 }
--- src/createProject.js
+++ src/createProject.js
@@ -10,7 +10,7 @@ import { log, createLoading } from "./utils";
 const repositoryName = "shop-sales-wx-app-next";
 const repositoryDir = path.resolve(process.cwd(), repositoryName);
 async function updateRepo() {
-  const loading = createLoading("仓库更新");
+  const loading = createLoading("项目代码更新");
   try {
     const result = await execa("git", ["pull", "--rebase"], {
       cwd: repositoryDir,
@@ -18,7 +18,7 @@ async function updateRepo() {
 
     if (result.failed) {
       loading.fail();
-      return Promise.reject(new Error("仓库更新失败"));
+      return Promise.reject(new Error("项目代码更新失败"));
     }
     loading.succeed();
   } catch (error) {
@@ -27,24 +27,25 @@ async function updateRepo() {
   }
 }
 
-// 克隆仓库
+// 克隆项目
 async function cloneRepo() {
-  const loading = createLoading("仓库下载");
+  const loading = createLoading("项目代码下载");
   const result = await execa("git", [
     "clone",
     "git@github.com:goldEli/shop-sales-wx-app-next.git",
   ]);
   if (result.failed) {
     loading.fail();
-    return Promise.reject(new Error("仓库下载失败"));
+    return Promise.reject(new Error("项目代码下载失败"));
   }
   loading.succeed();
 }
 
 // 安装依赖
-async function install() {
-  const loading = createLoading("仓库依赖安装");
-  const result = await execa("yarn", {
+async function install(useNpm) {
+  const loading = createLoading("项目依赖安装");
+  const commands = useNpm ? ["npm", ["install"]] : ["yarn"];
+  const result = await execa(...commands, {
     cwd: repositoryDir,
   });
   if (result.failed) {
@@ -55,29 +56,34 @@ async function install() {
 }
 
 // 启动
-async function start() {
-  const loading = createLoading("启动项目");
-  const result = await execa(
-    "taro",
-    ["build", "--type", "weapp", "--blended", "--watch"],
-    {
-      cwd: repositoryDir,
-    }
-  );
-  if (result.failed) {
-    loading.fail();
-    return Promise.reject(new Error("依赖安装失败"));
-  }
-  loading.succeed();
-}
+// async function start() {
+//   const loading = createLoading("启动项目");
+//   const result = await execa(
+//     "taro",
+//     ["build", "--type", "weapp", "--blended", "--watch"],
+//     {
+//       cwd: repositoryDir,
+//     }
+//   );
+//   if (result.failed) {
+//     loading.fail();
+//     return Promise.reject(new Error("依赖安装失败"));
+//   }
+//   loading.succeed();
+// }
 
-export async function createProject() {
-  console.log("===开始创建项目===");
+export async function createProject(options) {
+  console.log("   ");
+  console.log(`开始创建项目: ${repositoryName}`);
+  console.log("   ");
+  console.log("   ");
   if (fs.existsSync(repositoryDir)) {
     await updateRepo();
   } else {
     await cloneRepo();
   }
-  await install();
-//   await start()
+  await install(options.useNpm);
+  //   await start()
+  console.log("   ");
+  console.log("   ");
 }
